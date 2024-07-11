ae0fec5291c725dbc509a6c1e9a53ab0b3d88db9
ShobujDas <shobujd6.gmail.com>
2024-07-11 23:46:52 +0600

    Project finished

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -6,6 +6,9 @@ import Login from './pages/Login'
 import MyNavbar from './components/Navbar'
 import List from './pages/List'
 import Home from './pages/Home'
+import BookDetailPage from './pages/Detail'
+import ViewOrder from './pages/ViewOrder'
+import ViewOrderDetails from './pages/ViewOrderDetails'
 
 
 function App() {
@@ -19,6 +22,9 @@ function App() {
         <Route path="/login" element={ <Login/> } />
         <Route path="/register" element={ <Register/> } />
         <Route path="/book/list" element={ <List/> } />
+        <Route path="/book/view/:bookId" element={ <BookDetailPage/> } />
+        <Route path="/book/orders" element={ <ViewOrder/> } />
+        <Route path="/books/orders/:bookId" element={ <ViewOrderDetails/> } />
       </Routes> 
     </>
   )
--- Project/src/components/Card.jsx
+++ Project/src/components/Card.jsx
@@ -1,25 +1,33 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { useNavigate } from "react-router-dom";
 import { useFirebase } from "../context/Firebase";
 
 function BookCard(props) {
   const firebase = useFirebase();
+  const navigate = useNavigate();
   const [url, setURL] = useState(null);
 
   useEffect(() => {
-    firebase.getImageURL(props.imageURL).then(url => setURL(url))
-  },[])
+    firebase.getImageURL(props.imageURL).then((url) => setURL(url));
+  }, []);
 
   return (
-    <Card style={{ width: "18rem", margin:"15PX"}}>
-      <Card.Img variant="top" src={url } />
+    <Card style={{ width: "18rem", margin: "15PX" }}>
+      <Card.Img variant="top" src={url} />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
         <Card.Text>
-          This book has a title {props.name} and tihs book is sold by {props.displayName} and this book cost Tk.{props.price}
+          This book has a title {props.name} and tihs book is sold by{" "}
+          {props.displayName} and this book cost Tk.{props.price}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button
+          onClick={(e) => navigate(props.link)}
+          variant="primary"
+        >
+          View
+        </Button>
       </Card.Body>
     </Card>
   );
